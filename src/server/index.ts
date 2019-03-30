ae13c0a28d650d272cab2c46258a6bc5bb8dc774
begemot9i <daston2012@gmail.com>
2019-03-30 18:24:23 +0300

    replace ts-node by ts-node-dev, began data generation

--- src/server/index.ts
+++ src/server/index.ts
@@ -23,4 +23,53 @@ app.get('/', function(req: express.Request, res: express.Response) {
   `)
 })
 
+app.get('/data', function(req: express.Request, res: express.Response) {
+  try {
+    const dataset: Array<Array<number | string>> = []
+    const {
+      seriesCount: querySeriesCount = 1, dataCount: queryDataCount = 1, levelsCount: queryLevelsCount = 2
+    } = req.query
+    const params = [Number(querySeriesCount), Number(queryDataCount), Number(queryLevelsCount)]
+
+    if (params.some(param => typeof param !== 'number')) {
+      throw new Error('All params must be a valid number')
+    }
+
+    // Structure:
+    // [
+    //   [ ...[<Levels-naming array (length = levelsCount - 1)>], ...[<Labels array (length = dataCount)>] ],
+    //   [ ...[<Levels-indices array>], ...[Data values] ], - count = seriesCount * (levelsCount - 1)
+    //   ...e.t.c.
+    // ]
+
+
+    const [seriesCount, dataCount, levelsCount] = params
+    const baseLevelsNames = ['Серии', 'Агрегация серий']
+    const levelsNames = baseLevelsNames.slice(0, levelsCount - 1).reverse()
+    const labels = []
+
+    console.log(seriesCount, dataCount, levelsCount)
+
+    let begin = new Date(2019, 0, 1).getTime()
+
+    for (let i = dataCount; i > 0; i -= 1) {
+      begin -= 1000 * 60 * 60
+      const date = new Date(begin)
+      labels.push(`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}`)
+    }
+
+    dataset.push([...levelsNames, ...labels])
+
+    for (let level = levelsCount; level > 1; level -= 1) {}
+
+    res.json({
+      dataset
+    })
+  } catch (error) {
+    res.json({
+      error: new Error(error)
+    })
+  }
+})
+
 app.listen(3000)
